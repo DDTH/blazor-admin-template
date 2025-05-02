bbb3d57e5b9532d3860666e5f799f9ba078a55bf
Thanh Nguyen <btnguyen2k@gmail.com>
2025-05-03 02:43:06 +1000

    Restructure Demo project

--- content/Bat/Bat.Blazor/Bat.Blazor.Demo.App/Pages/ApplicationsAdd.razor.cs
+++ content/Bat/Bat.Blazor/Bat.Blazor.Demo.App/Pages/ApplicationsAdd.razor.cs
@@ -1,59 +1,61 @@
-// using Bat.Blazor.App.Shared;
-// using Bat.Shared.Api;
-// using Microsoft.AspNetCore.Components;
+﻿using Bat.Blazor.Demo.App.Shared;
+using Bat.Demo.Shared.Api;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.DependencyInjection;
 
-// namespace Bat.Blazor.App.Pages;
+namespace Bat.Blazor.Demo.App.Pages;
 
-// public partial class ApplicationsAdd
-// {
-// 	private string AlertMessage { get; set; } = string.Empty;
-// 	private string AlertType { get; set; } = "info";
-// 	private bool HideUI { get; set; } = false;
+public partial class ApplicationsAdd
+{
+	private string AlertMessage { get; set; } = string.Empty;
+	private string AlertType { get; set; } = "info";
+	private bool HideUI { get; set; } = false;
 
-// 	private string DisplayName { get; set; } = string.Empty;
-// 	private string PublicKeyPEM { get; set; } = string.Empty;
+	private string DisplayName { get; set; } = string.Empty;
+	private string PublicKeyPEM { get; set; } = string.Empty;
 
-// 	private void BtnClickCancel()
-// 	{
-// 		NavigationManager.NavigateTo(UIGlobals.ROUTE_APPLICATIONS);
-// 	}
+	private void BtnClickCancel()
+	{
+		NavigationManager.NavigateTo(DemoUIGlobals.ROUTE_APPLICATIONS);
+	}
 
-// 	private void ShowAlert(string type, string message)
-// 	{
-// 		AlertType = type;
-// 		AlertMessage = message;
-// 		StateHasChanged();
-// 	}
+	private void ShowAlert(string type, string message)
+	{
+		AlertType = type;
+		AlertMessage = message;
+		StateHasChanged();
+	}
 
-// 	private async Task BtnClickCreate()
-// 	{
-// 		HideUI = true;
-// 		ShowAlert("info", "Please wait...");
+	private async Task BtnClickCreate()
+	{
+		HideUI = true;
+		ShowAlert("info", "Please wait...");
 
-// 		// Validate display name
-// 		if (string.IsNullOrWhiteSpace(DisplayName))
-// 		{
-// 			HideUI = false;
-// 			ShowAlert("warning", "Display name is required.");
-// 			return;
-// 		}
+		// Validate display name
+		if (string.IsNullOrWhiteSpace(DisplayName))
+		{
+			HideUI = false;
+			ShowAlert("warning", "Display name is required.");
+			return;
+		}
 
-// 		var req = new CreateOrUpdateAppReq
-// 		{
-// 			DisplayName = DisplayName.Trim(),
-// 			PublicKeyPEM = PublicKeyPEM.Trim(),
-// 		};
-// 		var resp = await ApiClient.CreateAppAsync(req, await GetAuthTokenAsync(), ApiBaseUrl);
-// 		if (resp.Status != 200)
-// 		{
-// 			HideUI = false;
-// 			ShowAlert("danger", resp.Message!);
-// 			return;
-// 		}
-// 		ShowAlert("success", "Application created successfully. Navigating to applications list...");
-// 		var passAlertMessage = $"Application '{req.DisplayName}' created successfully.";
-// 		var passAlertType = "success";
-// 		await Task.Delay(500);
-// 		NavigationManager.NavigateTo($"{UIGlobals.ROUTE_APPLICATIONS}?alertMessage={passAlertMessage}&alertType={passAlertType}");
-// 	}
-// }
+		var req = new CreateOrUpdateAppReq
+		{
+			DisplayName = DisplayName.Trim(),
+			PublicKeyPEM = PublicKeyPEM.Trim(),
+		};
+		var apiClient = ServiceProvider.GetRequiredService<IDemoApiClient>();
+		var resp = await apiClient.CreateAppAsync(req, await GetAuthTokenAsync(), ApiBaseUrl);
+		if (resp.Status != 200)
+		{
+			HideUI = false;
+			ShowAlert("danger", resp.Message!);
+			return;
+		}
+		ShowAlert("success", "Application created successfully. Navigating to applications list...");
+		var passAlertMessage = $"Application '{req.DisplayName}' created successfully.";
+		var passAlertType = "success";
+		await Task.Delay(500);
+		NavigationManager.NavigateTo($"{DemoUIGlobals.ROUTE_APPLICATIONS}?alertMessage={passAlertMessage}&alertType={passAlertType}");
+	}
+}
