e01b86a7e99f17db12622630445d34e018ea867a
Thanh Nguyen <btnguyen2k@gmail.com>
2025-05-06 18:41:14 +1000

    Revert WasmBuildNative and RunAOTCompilation settings

--- content/init_projects.sh
+++ content/init_projects.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 BASENAME=Bat
+PWD=$(pwd)
 
 # Create a new solution
 dotnet new sln -o ${BASENAME}
@@ -14,10 +15,6 @@ dotnet new classlib -o ${BASENAME}.Shared.EF
 dotnet new webapi -o ${BASENAME}.Api
 dotnet new blazor -o ${BASENAME}.Blazor --interactivity Auto --all-interactive
 cd ${BASENAME}.Blazor && dotnet new razorclasslib -o ${BASENAME}.Blazor.App && cd ..
-## create demo projects
-dotnet new classlib -o ${BASENAME}.Demo.Shared
-dotnet new webapi -o ${BASENAME}.Demo.Api
-cd ${BASENAME}.Blazor && dotnet new razorclasslib -o ${BASENAME}.Blazor.Demo.App && cd ..
 
 # Add projects to solution
 ## add the Blazor (server) project first to make it the default project
@@ -29,10 +26,6 @@ dotnet sln add ${BASENAME}.Shared/${BASENAME}.Shared.csproj
 dotnet sln add ${BASENAME}.Shared.Api/${BASENAME}.Shared.Api.csproj
 dotnet sln add ${BASENAME}.Shared.EF/${BASENAME}.Shared.EF.csproj
 dotnet sln add ${BASENAME}.Api/${BASENAME}.Api.csproj
-## add demo projects
-dotnet sln add ${BASENAME}.Demo.Shared/${BASENAME}.Demo.Shared.csproj
-dotnet sln add ${BASENAME}.Demo.Api/${BASENAME}.Demo.Api.csproj
-dotnet sln add ${BASENAME}.Blazor/${BASENAME}.Blazor.Demo.App/${BASENAME}.Blazor.Demo.App.csproj
 
 # Add references
 dotnet add ${BASENAME}.Shared/${BASENAME}.Shared.csproj reference ${BASENAME}.Libs/${BASENAME}.Libs.csproj
@@ -45,13 +38,24 @@ dotnet add ${BASENAME}.Blazor/${BASENAME}.Blazor.Client/${BASENAME}.Blazor.Clien
 dotnet add ${BASENAME}.Blazor/${BASENAME}.Blazor/${BASENAME}.Blazor.csproj reference ${BASENAME}.Api/${BASENAME}.Api.csproj
 dotnet add ${BASENAME}.Blazor/${BASENAME}.Blazor/${BASENAME}.Blazor.csproj reference ${BASENAME}.Blazor/${BASENAME}.Blazor.App/${BASENAME}.Blazor.App.csproj
 dotnet add ${BASENAME}.Blazor/${BASENAME}.Blazor/${BASENAME}.Blazor.csproj reference ${BASENAME}.Blazor/${BASENAME}.Blazor.Client/${BASENAME}.Blazor.Client.csproj
-## demo projects
+cd ${PWD}
+
+## create demo projects
+cd ${BASENAME}
+dotnet new classlib -o ${BASENAME}.Demo.Shared
+dotnet new webapi -o ${BASENAME}.Demo.Api
+cd ${BASENAME}.Blazor && dotnet new razorclasslib -o ${BASENAME}.Blazor.Demo.App && cd ..
+## add demo projects to solution
+dotnet sln add ${BASENAME}.Demo.Shared/${BASENAME}.Demo.Shared.csproj
+dotnet sln add ${BASENAME}.Demo.Api/${BASENAME}.Demo.Api.csproj
+dotnet sln add ${BASENAME}.Blazor/${BASENAME}.Blazor.Demo.App/${BASENAME}.Blazor.Demo.App.csproj
+## demo projects references
 dotnet add ${BASENAME}.Demo.Shared/${BASENAME}.Demo.Shared.csproj reference ${BASENAME}.Shared/${BASENAME}.Shared.csproj
 dotnet add ${BASENAME}.Demo.Shared/${BASENAME}.Demo.Shared.csproj reference ${BASENAME}.Shared.EF/${BASENAME}.Shared.EF.csproj
 dotnet add ${BASENAME}.Demo.Api/${BASENAME}.Demo.Api.csproj reference ${BASENAME}.Demo.Shared/${BASENAME}.Demo.Shared.csproj
 dotnet add ${BASENAME}.Demo.Api/${BASENAME}.Demo.Api.csproj reference ${BASENAME}.Shared.Api/${BASENAME}.Shared.Api.csproj
 dotnet add ${BASENAME}.Blazor/${BASENAME}.Blazor.Demo.App/${BASENAME}.Blazor.Demo.App.csproj reference ${BASENAME}.Demo.Shared/${BASENAME}.Demo.Shared.csproj
-## core projects references demo projects
+## core projects --> demo projects
 dotnet add ${BASENAME}.Api/${BASENAME}.Api.csproj reference ${BASENAME}.Demo.Api/${BASENAME}.Demo.Api.csproj
 dotnet add ${BASENAME}.Blazor/${BASENAME}.Blazor/${BASENAME}.Blazor.csproj reference ${BASENAME}.Demo.Api/${BASENAME}.Demo.Api.csproj
 dotnet add ${BASENAME}.Blazor/${BASENAME}.Blazor/${BASENAME}.Blazor.csproj reference ${BASENAME}.Blazor/${BASENAME}.Blazor.Demo.App/${BASENAME}.Blazor.Demo.App.csproj
