9d1ec724fe404b4b7f0f3006aab5ec0e478f13cf
Thanh Nguyen <btnguyen2k@gmail.com>
2025-09-23 10:21:11 +1000

    Fix dark theme

--- content/Bat/Bat.Blazor/Bat.Blazor.App/Pages/Logout.razor.cs
+++ content/Bat/Bat.Blazor/Bat.Blazor.App/Pages/Logout.razor.cs
@@ -7,16 +7,10 @@ namespace Bat.Blazor.App.Pages;
 
 public partial class Logout : BaseComponent
 {
-	private string Message { get; set; } = "Logging out, please wait...";
-
-	// [Inject]
-	// private AuthenticationStateProvider AuthenticationStateProvider { get; set; } = default!;
-
 	protected override async Task OnAfterRenderAsync(bool firstRender)
 	{
 		var localStorage = ServiceProvider.GetRequiredService<LocalStorageHelper>();
 		await localStorage.RemoveItemAsync(Globals.LOCAL_STORAGE_KEY_AUTH_TOKEN);
-		// ((JwtAuthenticationStateProvider)AuthenticationStateProvider).NotifyStageChanged();
 
 		NavigationManager.NavigateTo(UIGlobals.ROUTE_LANDING, forceLoad: true);
 	}
