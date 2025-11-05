a7918ab0644399e28ec6039f34aad1f3942226ef
Thanh Nguyen <btnguyen2k@gmail.com>
2025-11-05 12:35:10 +1100

    Refactored Identity/Claims/Policies.

---
+++ content/Bat/Bat.Demo.Shared/Identity/DemoClaims.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Claims;
+using Bat.Shared.Identity;
+
+namespace Bat.Demo.Shared.Identity;
+
+public sealed class DemoClaims
+{
+	/// <summary>
+	/// Claim to mark a user/role as an application manager.
+	/// </summary>
+	public static readonly Claim CLAIM_ROLE_APPLICATION_MANAGER = new($"{BuiltinClaims.CLAIM_PREFIX}{BuiltinClaims.ROLE_PREFIX}", "application-manager");
+
+	/// <summary>
+	/// Permission to create a new application.
+	/// </summary>
+	public static readonly Claim CLAIM_PERM_CREATE_APPLICATION = new($"{BuiltinClaims.CLAIM_PREFIX}{BuiltinClaims.PERMISSION_PREFIX}", "application-create");
+
+	/// <summary>
+	/// Permission to delete an application.
+	/// </summary>
+	public static readonly Claim CLAIM_PERM_DELETE_APPLICATION = new($"{BuiltinClaims.CLAIM_PREFIX}{BuiltinClaims.PERMISSION_PREFIX}", "application-delete");
+
+	/// <summary>
+	/// Permission to modify an application.
+	/// </summary>
+	public static readonly Claim CLAIM_PERM_MODIFY_APPLICATION = new($"{BuiltinClaims.CLAIM_PREFIX}{BuiltinClaims.PERMISSION_PREFIX}", "application-modify");
+
+	public static readonly IEnumerable<Claim> ALL_CLAIMS = [
+		CLAIM_ROLE_APPLICATION_MANAGER,
+		CLAIM_PERM_CREATE_APPLICATION,
+		CLAIM_PERM_DELETE_APPLICATION,
+		CLAIM_PERM_MODIFY_APPLICATION,
+	];
+}
---
+++ content/Bat/Bat.Demo.Shared/Identity/DemoPolicies.cs
@@ -0,0 +1,51 @@
+﻿using Bat.Shared.Identity;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Bat.Demo.Shared.Identity;
+
+public sealed class DemoPolicies
+{
+	public const string POLICY_NAME_ADMIN_ROLE_OR_APPLICATION_MANAGER = "AdminRoleOrApplicationManager";
+	public static readonly AuthorizationPolicy POLICY_ADMIN_ROLE_OR_APPLICATION_MANAGER = new AuthorizationPolicyBuilder()
+		.RequireAuthenticatedUser()
+		.RequireAssertion(context =>
+		{
+			var hasAdminRole = context.User.HasClaim(BuiltinClaims.CLAIM_ROLE_GLOBAL_ADMIN.Type, BuiltinClaims.CLAIM_ROLE_GLOBAL_ADMIN.Value);
+			var hasApplicationManagerRole = context.User.HasClaim(DemoClaims.CLAIM_ROLE_APPLICATION_MANAGER.Type, DemoClaims.CLAIM_ROLE_APPLICATION_MANAGER.Value);
+			return hasAdminRole || hasApplicationManagerRole;
+		})
+		.Build();
+
+	public const string POLICY_NAME_ADMIN_ROLE_OR_CREATE_APP_PERM = "AdminRoleOrCreateAppPermission";
+	public static readonly AuthorizationPolicy POLICY_ADMIN_ROLE_OR_CREATE_APP_PERM = new AuthorizationPolicyBuilder()
+		.RequireAuthenticatedUser()
+		.RequireAssertion(context =>
+		{
+			var hasAdminRole = context.User.HasClaim(BuiltinClaims.CLAIM_ROLE_GLOBAL_ADMIN.Type, BuiltinClaims.CLAIM_ROLE_GLOBAL_ADMIN.Value);
+			var hasCreateAppPerm = context.User.HasClaim(DemoClaims.CLAIM_PERM_CREATE_APPLICATION.Type, DemoClaims.CLAIM_PERM_CREATE_APPLICATION.Value);
+			return hasAdminRole || hasCreateAppPerm;
+		})
+		.Build();
+
+	public const string POLICY_NAME_ADMIN_ROLE_OR_MODIFY_APP_PERM = "AdminRoleOrModifyAppPermission";
+	public static readonly AuthorizationPolicy POLICY_ADMIN_ROLE_OR_MODIFY_APP_PERM = new AuthorizationPolicyBuilder()
+		.RequireAuthenticatedUser()
+		.RequireAssertion(context =>
+		{
+			var hasAdminRole = context.User.HasClaim(BuiltinClaims.CLAIM_ROLE_GLOBAL_ADMIN.Type, BuiltinClaims.CLAIM_ROLE_GLOBAL_ADMIN.Value);
+			var hasModifyAppPerm = context.User.HasClaim(DemoClaims.CLAIM_PERM_MODIFY_APPLICATION.Type, DemoClaims.CLAIM_PERM_MODIFY_APPLICATION.Value);
+			return hasAdminRole || hasModifyAppPerm;
+		})
+		.Build();
+
+	public const string POLICY_NAME_ADMIN_ROLE_OR_DELETE_APP_PERM = "AdminRoleOrDeleteAppPermission";
+	public static readonly AuthorizationPolicy POLICY_ADMIN_ROLE_OR_DELETE_APP_PERM = new AuthorizationPolicyBuilder()
+		.RequireAuthenticatedUser()
+		.RequireAssertion(context =>
+		{
+			var hasAdminRole = context.User.HasClaim(BuiltinClaims.CLAIM_ROLE_GLOBAL_ADMIN.Type, BuiltinClaims.CLAIM_ROLE_GLOBAL_ADMIN.Value);
+			var hasDeleteAppPerm = context.User.HasClaim(DemoClaims.CLAIM_PERM_DELETE_APPLICATION.Type, DemoClaims.CLAIM_PERM_DELETE_APPLICATION.Value);
+			return hasAdminRole || hasDeleteAppPerm;
+		})
+		.Build();
+}
