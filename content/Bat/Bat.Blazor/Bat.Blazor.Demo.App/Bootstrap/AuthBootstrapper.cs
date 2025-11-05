23f0d52108b85dc096bdc39f44badcae81b38503
Thanh Nguyen <btnguyen2k@gmail.com>
2025-11-05 12:34:38 +1100

    Refactored Identity/Claims/Policies.

---
+++ content/Bat/Bat.Blazor/Bat.Blazor.Demo.App/Bootstrap/AuthBootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using Bat.Demo.Shared.Identity;
+using Bat.Shared.Bootstrap;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bat.Blazor.Demo.App.Bootstrap;
+
+[Bootstrapper]
+public class AuthBootstrapper
+{
+	public static void ConfigureServices(IServiceCollection services)
+	{
+		// set up authorization
+		services.AddAuthorizationCore(c =>
+		{
+			// Configurate authorization policies
+			c.AddPolicy(DemoPolicies.POLICY_NAME_ADMIN_ROLE_OR_APPLICATION_MANAGER, DemoPolicies.POLICY_ADMIN_ROLE_OR_APPLICATION_MANAGER);
+			c.AddPolicy(DemoPolicies.POLICY_NAME_ADMIN_ROLE_OR_CREATE_APP_PERM, DemoPolicies.POLICY_ADMIN_ROLE_OR_CREATE_APP_PERM);
+			c.AddPolicy(DemoPolicies.POLICY_NAME_ADMIN_ROLE_OR_MODIFY_APP_PERM, DemoPolicies.POLICY_ADMIN_ROLE_OR_MODIFY_APP_PERM);
+			c.AddPolicy(DemoPolicies.POLICY_NAME_ADMIN_ROLE_OR_DELETE_APP_PERM, DemoPolicies.POLICY_ADMIN_ROLE_OR_DELETE_APP_PERM);
+		});
+	}
+}
---
+++ content/Bat/Bat.Blazor/Bat.Blazor.Demo.App/Bootstrap/InitIdentityBootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using Bat.Demo.Shared.Identity;
+using Bat.Shared.Bootstrap;
+using Bat.Shared.Global;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Bat.Blazor.Demo.App.Bootstrap;
+
+[Bootstrapper(Priority = 100)]
+public class InitIdentityBootstrapper
+{
+	public static void ConfigureServices(IServiceCollection _, ILogger<InitIdentityBootstrapper> logger)
+	{
+		logger.LogInformation("Registering demo {count} claims...", DemoClaims.ALL_CLAIMS.Count());
+		GlobalRegistry.ALL_CLAIMS.UnionWith(DemoClaims.ALL_CLAIMS);
+		logger.LogInformation("Total registered claims: {count}", GlobalRegistry.ALL_CLAIMS.Count);
+	}
+}
