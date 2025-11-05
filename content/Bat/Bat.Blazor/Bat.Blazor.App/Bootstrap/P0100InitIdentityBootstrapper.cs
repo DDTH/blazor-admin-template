7d7be4d7d37b5e38136672574841f11f18a5a5e4
Thanh Nguyen <btnguyen2k@gmail.com>
2025-11-05 12:33:49 +1100

    Refactored Identity/Claims/Policies.

---
+++ content/Bat/Bat.Blazor/Bat.Blazor.App/Bootstrap/P0100InitIdentityBootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Bat.Shared.Bootstrap;
+using Bat.Shared.Global;
+using Bat.Shared.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Bat.Blazor.App.Bootstrap;
+
+/// <summary>
+/// Bootstrapper that register identity data.
+/// </summary>
+/// <remarks>
+///		Bootstrappers in Blazor.App project are shared between for Blazor Server and Blazor WASM.
+/// </remarks>
+[Bootstrapper(Priority = 100)]
+public class InitIdentityBootstrapper
+{
+	public static void ConfigureServices(IServiceCollection _, ILogger<InitIdentityBootstrapper> logger)
+	{
+		logger.LogInformation("Registering built-in {count} claims...", BuiltinClaims.ALL_CLAIMS.Count());
+		GlobalRegistry.ALL_CLAIMS.UnionWith(BuiltinClaims.ALL_CLAIMS);
+		logger.LogInformation("Total registered claims: {count}", GlobalRegistry.ALL_CLAIMS.Count);
+	}
+}
